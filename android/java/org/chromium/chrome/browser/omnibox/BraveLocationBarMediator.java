ff294b4f648b9b09f51ef0215c8d879c31d4c100
AlexeyBarabash <alexey@brave.com>
2026-02-01 20:14:59 +0200

    [cr146][Android] Create OmniboxChipManager and the open-in-app chip
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/466f936e51f26b1bb438f4f7bda2dd65a4b86c6e
    
    	[Link Capturing] Create OmniboxChipManager and the open-in-app chip
    
    	Low-Coverage-Reason: TESTS_IN_SEPARATE_CL implementation not at a point to test properly yet
    	Bug: 450253146
    	Change-Id: Id4296ce2ce685dcca9be535f8308a1295a1772a4
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362069

--- android/java/org/chromium/chrome/browser/omnibox/BraveLocationBarMediator.java
+++ android/java/org/chromium/chrome/browser/omnibox/BraveLocationBarMediator.java
@@ -77,7 +77,8 @@ public class BraveLocationBarMediator extends LocationBarMediator {
             @Nullable PageZoomIndicatorCoordinator pageZoomIndicatorCoordinator,
             FuseboxCoordinator fuseboxCoordinator,
             @Nullable MultiInstanceManager multiInstanceManager,
-            LocationBarEmbedder locationBarEmbedder) {
+            LocationBarEmbedder locationBarEmbedder,
+            @Nullable OmniboxChipManager omniboxChipManager) {
         super(
                 context,
                 locationBarLayout,
@@ -100,7 +101,8 @@ public class BraveLocationBarMediator extends LocationBarMediator {
                 pageZoomIndicatorCoordinator,
                 fuseboxCoordinator,
                 multiInstanceManager,
-                locationBarEmbedder);
+                locationBarEmbedder,
+                omniboxChipManager);
     }
 
     public static Class<OmniboxUma> getOmniboxUmaClass() {
