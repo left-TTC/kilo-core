brave/brave-core

[Shields UI] Add updated Shields panel front-end

components/brave_shields/resources/panel_new/components/adblock_only_prompt.tsx
@@ -1,0 +1,110 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+import Button from '@brave/leo/react/button'
+
+import { useAppState, useAppActions } from './app_context'
+import { getString } from '../lib/strings'
+import { formatString } from '$web-common/formatString'
+
+import { style } from './adblock_only_prompt.style'
+
+const learnMoreURL =
+  'https://support.brave.app/hc/en-us/articles/38076796692109'
+
+export function AdblockOnlyPrompt() {
+  const shieldsEnabled = useAppState(
+    (s) => s.siteBlockInfo.isBraveShieldsEnabled,
+  )
+  const adblockOnlyEnabled = useAppState(
+    (s) => s.siteBlockInfo.isBraveShieldsAdBlockOnlyModeEnabled,
+  )
+  const showDisabledPrompt = useAppState(
+    (s) => s.siteBlockInfo.showShieldsDisabledAdBlockOnlyModePrompt,
+  )
+  const repeatedReloadsDetected = useAppState((s) => s.repeatedReloadsDetected)
+
+  if (!adblockOnlyEnabled && shieldsEnabled && repeatedReloadsDetected) {
+    return <AdblockOnlyEnablePrompt />
+  }
+
+  if (!adblockOnlyEnabled && !shieldsEnabled && showDisabledPrompt) {
+    return <AdblockOnlyEnablePrompt />
+  }
+
+  if (shieldsEnabled && adblockOnlyEnabled) {
+    return <AdblockOnlyFeedbackPrompt />
+  }
+
+  return null
+}
+
+function AdblockOnlyEnablePrompt() {
+  const actions = useAppActions()
+  return (
+    <div data-css-scope={style.scope}>
+      <h4>
+        {getString(
+          'BRAVE_SHIELDS_ARE_YOU_EXPERIENCING_ISSUES_WITH_THIS_SITE_TITLE',
+        )}
+      </h4>
+      <p>
+        {formatString(
+          getString(
+            'BRAVE_SHIELDS_ARE_YOU_EXPERIENCING_ISSUES_WITH_THIS_SITE_DESC1',
+          ),
+          {
+            $1: (content) => (
+              <button onClick={() => actions.openTab(learnMoreURL)}>
+                {content}
+              </button>
+            ),
+          },
+        )}
+      </p>
+      <div className='actions'>
+        <Button onClick={() => actions.setAdBlockOnlyModeEnabled(true)}>
+          {getString('BRAVE_SHIELDS_ENABLE_AD_BLOCK_ONLY_MODE')}
+        </Button>
+        <Button
+          kind='plain'
+          onClick={actions.dismissAdBlockOnlyModePrompt}
+        >
+          {getString('BRAVE_SHIELDS_DISMISS_ALERT')}
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+function AdblockOnlyFeedbackPrompt() {
+  const actions = useAppActions()
+  return (
+    <div data-css-scope={style.scope}>
+      <h4>
+        {getString('BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_TITLE')}
+      </h4>
+      <p>
+        {getString('BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_DESC')}
+      </p>
+      <div className='actions'>
+        <Button onClick={actions.closeUI}>
+          {getString(
+            'BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_LOOKS_GOOD',
+          )}
+        </Button>
+        <Button
+          kind='plain'
+          onClick={actions.openWebCompatReporter}
+        >
+          {getString(
+            'BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_REPORT_SITE',
+          )}
+        </Button>
+      </div>
+    </div>
+  )
+}

components/brave_shields/resources/panel_new/components/ads_blocked_details.tsx
@@ -1,0 +1,33 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+
+import { useAppState } from './app_context'
+import { getString } from '../lib/strings'
+import { DetailsHeader } from './details_header'
+import { ResourceListView } from './resource_list_view'
+
+import { style } from './ads_blocked_details.style'
+
+interface Props {
+  onBack: () => void
+}
+
+export function AdsBlockedDetails(props: Props) {
+  const adsList = useAppState((s) => s.siteBlockInfo.adsList)
+  const title = getString('BRAVE_SHIELDS_TRACKERS_ADS_BLOCKED_TITLE')
+  return (
+    <main data-css-scope={style.scope}>
+      <DetailsHeader
+        title={`${title} (${adsList.length})`}
+        onBack={props.onBack}
+      />
+      <div className='resource-list scrollable'>
+        <ResourceListView urls={adsList.map((url) => url.url)} />
+      </div>
+    </main>
+  )
+}

components/brave_shields/resources/panel_new/components/details_header.tsx
@@ -1,0 +1,36 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+import Icon from '@brave/leo/react/icon'
+
+import { useAppState } from './app_context'
+
+import { style } from './details_header.style'
+
+interface Props {
+  title: string
+  onBack: () => void
+  children?: React.ReactNode
+}
+
+export function DetailsHeader(props: Props) {
+  const host = useAppState((s) => s.siteBlockInfo.host)
+
+  return (
+    <div data-css-scope={style.scope}>
+      <div className='header'>
+        <button onClick={props.onBack}>
+          <Icon name='arrow-left' />
+        </button>
+        <div className='text'>
+          <h4>{props.title}</h4>
+          <div className='host'>{host}</div>
+        </div>
+      </div>
+      {props.children}
+    </div>
+  )
+}

components/brave_shields/resources/panel_new/components/scripts_blocked_details.tsx
@@ -1,0 +1,74 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+
+import { useAppState, useAppActions } from './app_context'
+import { getString } from '../lib/strings'
+import { DetailsHeader } from './details_header'
+import { ResourceListView } from './resource_list_view'
+
+import { style } from './scripts_blocked_details.style'
+
+interface Props {
+  onBack: () => void
+}
+
+export function ScriptsBlockedDetails(props: Props) {
+  const actions = useAppActions()
+  const allowedList = useAppState((s) =>
+    s.siteBlockInfo.allowedJsList.map((item) => item.url),
+  )
+  const blockedList = useAppState((s) =>
+    s.siteBlockInfo.blockedJsList.map((item) => item.url),
+  )
+
+  return (
+    <main data-css-scope={style.scope}>
+      <DetailsHeader
+        title={getString('BRAVE_SHIELDS_SCRIPT_DETAILS_TITLE')}
+        onBack={props.onBack}
+      />
+      <div className='scrollable'>
+        {allowedList.length > 0 && (
+          <>
+            <div className='list-header allow-header'>
+              {getString('BRAVE_SHIELDS_ALLOWED_SCRIPTS_LABEL')} (
+              {allowedList.length})
+              <button onClick={() => actions.blockAllowedScripts(allowedList)}>
+                {getString('BRAVE_SHIELDS_BLOCK_SCRIPTS_ALL')}
+              </button>
+            </div>
+            <div className='resource-list'>
+              <ResourceListView
+                urls={allowedList}
+                actionText={getString('BRAVE_SHIELDS_SCRIPT_BLOCK')}
+                actionHandler={(url) => actions.blockAllowedScripts([url])}
+              />
+            </div>
+          </>
+        )}
+        {blockedList.length > 0 && (
+          <>
+            <div className='list-header block-header'>
+              {getString('BRAVE_SHIELDS_BLOCKED_SCRIPTS_LABEL')} (
+              {blockedList.length})
+              <button onClick={() => actions.allowScriptsOnce(blockedList)}>
+                {getString('BRAVE_SHIELDS_ALLOW_SCRIPTS_ALL')}
+              </button>
+            </div>
+            <div className='resource-list'>
+              <ResourceListView
+                urls={blockedList}
+                actionText={getString('BRAVE_SHIELDS_ALLOW_SCRIPT_ONCE')}
+                actionHandler={(url) => actions.allowScriptsOnce([url])}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </main>
+  )
+}

left-TTC/kilo-core

components/brave_shields/resources/panel_new/components/adblock_only_prompt.tsx
@@ -1,0 +1,112 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+import Button from '@brave/leo/react/button'
+
+import { useShieldsState, useShieldsActions } from '../lib/shields_context'
+import { getString } from '../lib/strings'
+import { formatString } from '$web-common/formatString'
+
+import { style } from './adblock_only_prompt.style'
+
+const learnMoreURL =
+  'https://support.brave.app/hc/en-us/articles/38076796692109'
+
+export function AdblockOnlyPrompt() {
+  const shieldsEnabled = useShieldsState(
+    (s) => s.siteBlockInfo.isBraveShieldsEnabled,
+  )
+  const adblockOnlyEnabled = useShieldsState(
+    (s) => s.siteBlockInfo.isBraveShieldsAdBlockOnlyModeEnabled,
+  )
+  const showDisabledPrompt = useShieldsState(
+    (s) => s.siteBlockInfo.showShieldsDisabledAdBlockOnlyModePrompt,
+  )
+  const repeatedReloadsDetected = useShieldsState(
+    (s) => s.repeatedReloadsDetected,
+  )
+
+  if (!adblockOnlyEnabled && shieldsEnabled && repeatedReloadsDetected) {
+    return <AdblockOnlyEnablePrompt />
+  }
+
+  if (!adblockOnlyEnabled && !shieldsEnabled && showDisabledPrompt) {
+    return <AdblockOnlyEnablePrompt />
+  }
+
+  if (shieldsEnabled && adblockOnlyEnabled) {
+    return <AdblockOnlyFeedbackPrompt />
+  }
+
+  return null
+}
+
+function AdblockOnlyEnablePrompt() {
+  const actions = useShieldsActions()
+  return (
+    <div data-css-scope={style.scope}>
+      <h4>
+        {getString(
+          'BRAVE_SHIELDS_ARE_YOU_EXPERIENCING_ISSUES_WITH_THIS_SITE_TITLE',
+        )}
+      </h4>
+      <p>
+        {formatString(
+          getString(
+            'BRAVE_SHIELDS_ARE_YOU_EXPERIENCING_ISSUES_WITH_THIS_SITE_DESC1',
+          ),
+          {
+            $1: (content) => (
+              <button onClick={() => actions.openTab(learnMoreURL)}>
+                {content}
+              </button>
+            ),
+          },
+        )}
+      </p>
+      <div className='actions'>
+        <Button onClick={() => actions.setAdBlockOnlyModeEnabled(true)}>
+          {getString('BRAVE_SHIELDS_ENABLE_AD_BLOCK_ONLY_MODE')}
+        </Button>
+        <Button
+          kind='plain'
+          onClick={actions.dismissAdBlockOnlyModePrompt}
+        >
+          {getString('BRAVE_SHIELDS_DISMISS_ALERT')}
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+function AdblockOnlyFeedbackPrompt() {
+  const actions = useShieldsActions()
+  return (
+    <div data-css-scope={style.scope}>
+      <h4>
+        {getString('BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_TITLE')}
+      </h4>
+      <p>
+        {getString('BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_DESC')}
+      </p>
+      <div className='actions'>
+        <Button onClick={actions.closeUI}>
+          {getString(
+            'BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_LOOKS_GOOD',
+          )}
+        </Button>
+        <Button
+          kind='plain'
+          onClick={actions.openWebCompatReporter}
+        >
+          {getString(
+            'BRAVE_SHIELDS_IS_THIS_SITE_WORKING_CORRECTLY_NOW_REPORT_SITE',
+          )}
+        </Button>
+      </div>
+    </div>
+  )
+}

components/brave_shields/resources/panel_new/components/ads_blocked_details.tsx
@@ -1,0 +1,33 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+
+import { useShieldsState } from '../lib/shields_context'
+import { getString } from '../lib/strings'
+import { DetailsHeader } from './details_header'
+import { ResourceListView } from './resource_list_view'
+
+import { style } from './ads_blocked_details.style'
+
+interface Props {
+  onBack: () => void
+}
+
+export function AdsBlockedDetails(props: Props) {
+  const adsList = useShieldsState((s) => s.siteBlockInfo.adsList)
+  const title = getString('BRAVE_SHIELDS_TRACKERS_ADS_BLOCKED_TITLE')
+  return (
+    <main data-css-scope={style.scope}>
+      <DetailsHeader
+        title={`${title} (${adsList.length})`}
+        onBack={props.onBack}
+      />
+      <div className='resource-list scrollable'>
+        <ResourceListView urls={adsList.map((url) => url.url)} />
+      </div>
+    </main>
+  )
+}

components/brave_shields/resources/panel_new/components/details_header.tsx
@@ -1,0 +1,36 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+import Icon from '@brave/leo/react/icon'
+
+import { useShieldsState } from '../lib/shields_context'
+
+import { style } from './details_header.style'
+
+interface Props {
+  title: string
+  onBack: () => void
+  children?: React.ReactNode
+}
+
+export function DetailsHeader(props: Props) {
+  const host = useShieldsState((s) => s.siteBlockInfo.host)
+
+  return (
+    <div data-css-scope={style.scope}>
+      <div className='header'>
+        <button onClick={props.onBack}>
+          <Icon name='arrow-left' />
+        </button>
+        <div className='text'>
+          <h4>{props.title}</h4>
+          <div className='host'>{host}</div>
+        </div>
+      </div>
+      {props.children}
+    </div>
+  )
+}

components/brave_shields/resources/panel_new/components/scripts_blocked_details.tsx
@@ -1,0 +1,74 @@
+/* Copyright (c) 2026 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+
+import { useShieldsState, useShieldsActions } from '../lib/shields_context'
+import { getString } from '../lib/strings'
+import { DetailsHeader } from './details_header'
+import { ResourceListView } from './resource_list_view'
+
+import { style } from './scripts_blocked_details.style'
+
+interface Props {
+  onBack: () => void
+}
+
+export function ScriptsBlockedDetails(props: Props) {
+  const actions = useShieldsActions()
+  const allowedList = useShieldsState((s) =>
+    s.siteBlockInfo.allowedJsList.map((item) => item.url),
+  )
+  const blockedList = useShieldsState((s) =>
+    s.siteBlockInfo.blockedJsList.map((item) => item.url),
+  )
+
+  return (
+    <main data-css-scope={style.scope}>
+      <DetailsHeader
+        title={getString('BRAVE_SHIELDS_SCRIPT_DETAILS_TITLE')}
+        onBack={props.onBack}
+      />
+      <div className='scrollable'>
+        {allowedList.length > 0 && (
+          <>
+            <div className='list-header allow-header'>
+              {getString('BRAVE_SHIELDS_ALLOWED_SCRIPTS_LABEL')} (
+              {allowedList.length})
+              <button onClick={() => actions.blockAllowedScripts(allowedList)}>
+                {getString('BRAVE_SHIELDS_BLOCK_SCRIPTS_ALL')}
+              </button>
+            </div>
+            <div className='resource-list'>
+              <ResourceListView
+                urls={allowedList}
+                actionText={getString('BRAVE_SHIELDS_SCRIPT_BLOCK')}
+                actionHandler={(url) => actions.blockAllowedScripts([url])}
+              />
+            </div>
+          </>
+        )}
+        {blockedList.length > 0 && (
+          <>
+            <div className='list-header block-header'>
+              {getString('BRAVE_SHIELDS_BLOCKED_SCRIPTS_LABEL')} (
+              {blockedList.length})
+              <button onClick={() => actions.allowScriptsOnce(blockedList)}>
+                {getString('BRAVE_SHIELDS_ALLOW_SCRIPTS_ALL')}
+              </button>
+            </div>
+            <div className='resource-list'>
+              <ResourceListView
+                urls={blockedList}
+                actionText={getString('BRAVE_SHIELDS_ALLOW_SCRIPT_ONCE')}
+                actionHandler={(url) => actions.allowScriptsOnce([url])}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </main>
+  )
+}